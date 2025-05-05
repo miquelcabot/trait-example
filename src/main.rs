319f540d5279de326cfb9b1f7ec3270d1e81aaad
miquelcabot <miquel.cabot@gmail.com>
2025-05-05 16:42:26 +0200

    Add VotingModule with voting functionality and tests

--- src/main.rs
+++ src/main.rs
@@ -49,3 +49,30 @@ fn test_step2() {
     assert_eq!(balances.balance(1), 50);
     assert_eq!(balances.balance(2), 250);
 }
+
+#[test]
+fn test_step_3() {
+    let mut voting = step3::VotingModule::new();
+    voting.vote(1, 0, true);
+    voting.vote(2, 0, false);
+
+    assert!(voting.get_vote(1, 0) == true);
+    assert!(voting.get_vote(2, 0) == false);
+
+    assert!(voting.get_vote(1, 1) == false);
+    assert!(voting.get_vote(2, 1) == false);
+}
+
+// // Account ID in step 2 and step 3 are defined differently!
+// fn wont_work() {
+// 	let user_1 = 1;
+// 	let user_2 = 2;
+// 	let mut balances = step2::BalancesModule::new();
+// 	let mut voting = step3::VotingModule::new();
+
+// 	balances.set_balance(user_1, 100);
+// 	balances.set_balance(user_2, 200);
+
+// 	voting.vote(user_1, 0, true);
+// 	voting.vote(user_2, 0, false);
+// }
